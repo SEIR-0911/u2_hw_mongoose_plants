f3c689da8a60df9dcae7cdd665d430e42ebc4e06
ahunter623 <ahunter623@gmail.com>
2023-10-09 12:34:13 -0400

    1st Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('./db');
+const plantsController = require('./controllers/plantController')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+// require() imports and middleware here ^ ///////
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+// app.use() middleware here ^ ///////////////////
+
+app.get('/', (req, res) => res.send('This is root!'))
+app.get('/plants', plantsController.getAllPlants)
+app.get('/plants/:id', plantsController.getPlantById)
+app.post('/plants', plantsController.createPlant)
+app.put('/plants/:id', plantsController.updatePlant)
+app.delete('/plants/:id', plantsController.deletePlant)
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
